0a093ad7709a9628c325fcb568e29c34d59b888b
Paul Kreymborg <paul.kreymborg@softwareag.com>
2019-01-02 15:04:26 -0800

    Made more changes for cli

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿
+using CommandLine;
+
+namespace ny_cli {
+    [Verb("add", HelpText = "Add file contents to the index.")]
+    class AddOptions {
+        //normal options here
+    }
+    [Verb("commit", HelpText = "Record changes to the repository.")]
+    class CommitOptions {
+        //commit options here
+    }
+    [Verb("clone", HelpText = "Clone a repository into a new directory.")]
+    class CloneOptions {
+        //clone options here
+    }
+    
+    class Program {
+        // internal class NyokaOptions {
+        //     [Option('v', "verbose", Required = false, HelpText = "Set output to verbose messages.")]
+        //     public bool Verbose { get; set; }
+        // }
+        
+        static void Main(string[] args) {
+            Parser.Default.ParseArguments<AddOptions, CommitOptions, CloneOptions>(args)
+                .MapResult(
+                    (AddOptions opts) => {
+                        System.Console.WriteLine("Add");
+                        return 1;
+                    },
+                    (CommitOptions opts) => {
+                        System.Console.WriteLine("Commit");
+                        return 1;
+                    },
+                    (CloneOptions opts) => {
+                        System.Console.WriteLine("Clone");
+                        return 1;
+                    },
+                    errs => 1
+                );
+        }
+    }
+}
