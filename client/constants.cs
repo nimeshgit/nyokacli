144b511174d435df3330be4c3002484827ec40bc
Paul Kreymborg <paulkreymborg@Pauls-MacBook-Pro.local>
2019-01-14 20:22:00 -0800

    Removed more dead code

--- client/constants.cs
+++ client/constants.cs
@@ -8,25 +8,4 @@ namespace Constants
         internal const string RESOURCE_TYPE_HINT = "Resource Type: \"code\", \"data\" or \"model\"";
         internal const string APPLICATION_ALIAS = "nyoka";
     }
-
-    // public class ResourceIdentifier
-    // {
-    //     public string resourceName;
-    //     public string version;
-    //     public ResourceType resourceType;
-
-    //     public ResourceIdentifier(string resourceName, ResourceType resourceType)
-    //     {
-    //         this.resourceName = resourceName;
-    //         this.resourceType = resourceType;
-    //         this.version = null;
-    //     }
-
-    //     public ResourceIdentifier(string resourceName, ResourceType resourceType, string version)
-    //     {
-    //         this.resourceName = resourceName;
-    //         this.resourceType = resourceType;
-    //         this.version = version;
-    //     }
-    // }
 }
