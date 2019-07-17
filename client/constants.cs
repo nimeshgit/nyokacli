abbf20cdb9c03248c20c0fde6b5b4c656ccbc8b1
Nimesh <38841604+nimeshgit@users.noreply.github.com>
2019-07-17 14:53:44 +0530

    Added constants for console output table headers

--- client/constants.cs
+++ client/constants.cs
@@ -7,5 +7,13 @@ namespace Constants
     {
         internal const string RESOURCE_TYPE_HINT = "Resource Type: \"code\", \"data\" or \"model\"";
         internal const string APPLICATION_ALIAS = "nyoka";
+
+        internal const string HeaderStringType = " Type";
+        internal const string HeaderStringNameOfResource = " Name of Resource";
+        internal const string HeaderStringLatestVersion = " Latest Version";
+        internal const string HeaderStringLocalVersion = " Local Version";
+        internal const string HeaderStringFileSize = " File Size";
+        internal const string HeaderStringVersion = " Version";
+
     }
 }
